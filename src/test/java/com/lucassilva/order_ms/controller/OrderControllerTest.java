b1c1db879fcb79ad5ba11e20525f8bd9bdaebe9e
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-10-01 17:08:19 -0300

    test: adds more unit tests to order controller

--- src/test/java/com/lucassilva/order_ms/controller/OrderControllerTest.java
+++ src/test/java/com/lucassilva/order_ms/controller/OrderControllerTest.java
@@ -12,12 +12,15 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
@@ -32,6 +35,9 @@ class OrderControllerTest {
 
     @Captor
     ArgumentCaptor<Long> customerIdCaptor;
+
+    @Captor
+    ArgumentCaptor<PageRequest> pageRequestCaptor;
 
     @Nested
     class ListOrders {
@@ -54,7 +60,7 @@ class OrderControllerTest {
             var customerId = 1L;
             var page = 0;
             var size = 10;
-            doReturn(OrderResponseFactory.buildWithOneItem()).when(orderService).findAllByCustomerId(customerIdCaptor.capture(), any());
+            doReturn(OrderResponseFactory.buildWithOneItem()).when(orderService).findAllByCustomerId(customerIdCaptor.capture(), pageRequestCaptor.capture());
             doReturn(BigDecimal.valueOf(20.50)).when(orderService).findTotalOnOrdersByCustomerId(customerIdCaptor.capture());
 
             ResponseEntity<ApiResponse<OrderResponse>> response = orderController.listOrders(customerId, page, size);
@@ -62,10 +68,36 @@ class OrderControllerTest {
             assertEquals(2, customerIdCaptor.getAllValues().size());
             assertEquals(customerId, customerIdCaptor.getAllValues().getFirst());
             assertEquals(customerId, customerIdCaptor.getAllValues().get(1));
+            assertEquals(page, pageRequestCaptor.getValue().getPageNumber());
+            assertEquals(size, pageRequestCaptor.getValue().getPageSize());
         }
 
         @Test
         void shouldReturnResponseBodyCorrectly() {
+            var customerId = 1L;
+            var page = 0;
+            var size = 10;
+            BigDecimal totalOnOrders = BigDecimal.valueOf(20.50);
+            Page<OrderResponse> pagination = OrderResponseFactory.buildWithOneItem();
+
+            doReturn(pagination).when(orderService).findAllByCustomerId(anyLong(), any());
+            doReturn(totalOnOrders).when(orderService).findTotalOnOrdersByCustomerId(anyLong());
+
+            ResponseEntity<ApiResponse<OrderResponse>> response = orderController.listOrders(customerId, page, size);
+
+            assertNotNull(response);
+            assertNotNull(response.getBody());
+            assertNotNull(response.getBody().data());
+            assertNotNull(response.getBody().pagination());
+            assertNotNull(response.getBody().summary());
+
+            assertEquals(totalOnOrders, response.getBody().summary().get("totalOnOrders"));
+            assertEquals(pagination.getTotalElements(), response.getBody().pagination().totalItems());
+            assertEquals(pagination.getTotalPages(), response.getBody().pagination().totalPages());
+            assertEquals(pagination.getNumber(), response.getBody().pagination().page());
+            assertEquals(pagination.getSize(), response.getBody().pagination().size());
+
+            assertEquals(pagination.getContent(), response.getBody().data());
         }
     }
 }
